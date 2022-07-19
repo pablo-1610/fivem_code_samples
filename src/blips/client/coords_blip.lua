e4bf8a54c645e0323e3db30dd35955ec0cefc8fb
Wezor <68116801+Wz-FiveM@users.noreply.github.com>
2022-07-19 16:47:09 +0200

    :sparkles: Create blips
    * :zap: Add Blips
    
    * :zap: Change Blips Event to RegisterCommand
    
    * :bug: Fix code
    
    Co-authored-by: Pablo Zapata <pablo.zapata.dev@gmail.com>

---
+++ src/blips/client/coords_blip.lua
@@ -0,0 +1,33 @@
+-- Credits: Wezor
+--[[
+    Used natives:
+        - GetEntityCoords (https://docs.fivem.net/natives/?_0x1647F1CB)
+        - PlayerPedId (https://docs.fivem.net/natives/?_0xD80958FC74E988A6)
+        - AddBlipForCoord (https://docs.fivem.net/natives/?_0xC6F43D0E)
+        - SetBlipSprite (https://docs.fivem.net/natives/?_0xDF735600A4696DAF)
+        - SetBlipColour (https://docs.fivem.net/natives/?_0x03D7FB09E75D6B7E)
+        - SetBlipScale (https://docs.fivem.net/natives/?_0xD38744167B2FA257)
+        - BeginTextCommandSetBlipName (https://docs.fivem.net/natives/?_0xF9113A30DE5C6670)
+        - AddTextComponentString (NATIVE INVALID IN WEBSITE)
+        - EndTextCommandSetBlipName (https://docs.fivem.net/natives/?_0xBC38B49BCB83BC9B)
+--]]
+
+RegisterCommand("coords_blip", function(_, args)
+    if (#args < 4) then
+        print("Usage: coords_blip <sprite> <name> <scale> <color>")
+        return
+    end
+    local coords <const> = GetEntityCoords(PlayerPedId());
+    local sprite <const> = args[2];
+    local name <const> = args[3];
+    local scale <const> = args[4];
+    local color <const> = args[5];
+    local blips <const> = AddBlipForCoord(coords.x, coords.y, coords.z);
+
+    SetBlipSprite(blips, sprite);
+    SetBlipColour(blips, color);
+    SetBlipScale(blips, scale);
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentString(name);
+    EndTextCommandSetBlipName(blips);
+end)
