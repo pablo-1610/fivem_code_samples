b44029b73a468cfc82922f3d842eba6d51e99771
Wezor <68116801+Wz-FiveM@users.noreply.github.com>
2022-07-19 17:09:06 +0200

    :sparkles: Add serializeWithSpaces to json
    * :zap: Add Blips
    
    Co-authored-by: Pablo Zapata <pablo.zapata.dev@gmail.com>

---
+++ src/json/shared/serializeWithSpaces.lua
@@ -0,0 +1,25 @@
+-- Credits : Wezor
+
+local EXAMPLE_TABLE <const> = {
+    NYC = {
+        latitude = 40.7128,
+        longitude = -74.0060,
+        name = "New York City",
+        population = 8406000,
+    },
+
+    LAX = {
+        latitude = 34.0522,
+        longitude = -118.2437,
+        name = "Los Angeles",
+        population = 3884307,
+    },
+}
+
+RegisterCommand("show_my_table_indent", function()
+    local serializedTable <const> = json.encode(EXAMPLE_TABLE, { indent = true })
+    print("Serialized:", serializedTable)
+
+    local deserializedTable <const> = json.decode(tidyTable)
+    print("Deserialized example:", deserializedTable.LAX.name)
+end)
