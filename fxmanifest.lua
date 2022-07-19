4b8e10715872f67a5b620d172fc5b9022c7f8c73
Pablo Zapata <pablo.zapata.dev@gmail.com>
2022-07-19 14:55:02 +0200

    Add project manifest

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'John Doe <j.doe@example.com>'
+description 'This is a showcase script'
+version '1.0.0'
+
+files { "data/*.json" }
+
+client_scripts { "src/**/client/*.lua" }
+server_scripts { "src/**/server/*.lua" }
+shared_scripts { "src/**/shared/*.lua" }
