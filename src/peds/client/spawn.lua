b866ff1140ba9bb653492bc5acc01817e232cd75
Pablo ZAPATA <pablo.zapata@epitech.eu>
2022-11-03 14:42:05 +0100

    Fix test reports

--- src/peds/client/spawn.lua
+++ src/peds/client/spawn.lua
@@ -1,13 +1,12 @@
 RegisterCommand("spawn_ped", function(_, args)
-    local model, ped = nil, nil
+    local model = GetHashKey(args[1])
     if (#args ~= 1) then
         return print("Usage: /spawn_ped [model]")
     end
-    model = GetHashKey(args[1])
     RequestModel(model)
     while (not HasModelLoaded(model)) do
         Wait(100)
     end
-    ped = CreatePed(9, model, GetEntityCoords(PlayerPedId()), GetEntityHeading(PlayerPedId()))
+    CreatePed(9, model, GetEntityCoords(PlayerPedId()), GetEntityHeading(PlayerPedId()))
     SetModelAsNoLongerNeeded(model)
 end)
